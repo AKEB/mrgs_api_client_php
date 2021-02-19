34367e4a80fffe2dba58ecbd4e962afa94d139b3
Vadim Babajanyan <babadzhanyan@corp.mail.ru>
2021-02-19 11:33:17 +0300

    TEST env

--- tests/AutorizeTest.php
+++ tests/AutorizeTest.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 class AutorizeTest extends PHPUnit\Framework\TestCase {
 
 	public function testAccessToken() {
-		getenv('MRGS_APP_ID');
+		var_dump(getenv('MRGS_APP_ID'));
 		var_export($_ENV);
 
 	}
